1bd30546fe1fe78d269030c7553480d5ed42d4d1
Emmabata <ndinezeemmanuel@gmail.com>
2023-03-25 18:47:49 +0100

    new

--- script.js
+++ script.js
@@ -1,102 +1,75 @@
-// const favCart = document.querySelectorAll(".fa-heart");
-// const remCart = document.querySelectorAll(".fa-trash");
-// const addCart = document.querySelectorAll(".fa-plus");
-// const minusCart = document.querySelectorAll(".fa-minus");
-// const input = document.getElementById("vaLue");
+const favCart = document.querySelectorAll(".fa-heart");
+const remCart = document.querySelectorAll(".fa-trash");
 
+//
+const plusBtn1 = document.querySelector(".plus1");
+const plusBtn2 = document.querySelector(".plus2");
+const plusBtn3 = document.querySelector(".plus3");
+//
+const minusBtn1 = document.querySelector(".minus1");
+const minusBtn2 = document.querySelector(".minus2");
+const minusBtn3 = document.querySelector(".minus3");
+//
+const input1 = document.getElementById("vaLue1");
+const input2 = document.getElementById("vaLue2");
+const input3 = document.getElementById("vaLue3");
+//
+const priceTotal1 = document.getElementsByClassName("total-price1");
+const priceTotal2 = document.getElementsByClassName("total-price2");
+const priceTotal3 = document.getElementsByClassName("total-price3");
 
-// console.log(input.value);
-// console.log(val.value);
-// console.log(addCart);
-// console.log(minusCart);
-
-// favCart.forEach(box => {
-//     box.addEventListener('click', (event) => {
-//       console.log('box clicked', event);
-//       if(box.style.color == "black"){
-//       box.setAttribute('style', 'color: red;');
-//       }else {
-//         box.setAttribute('style', 'color: black;');
-//       }
-//     });
-//   });
-
-// // FOR THE INCREEMENT BUTTON
-//  addCart.forEach(plusSign => {
-//   plusSign.addEventListener("click", () => {
-//     input.value = parseInt(input.value) + 1;
-//   } )
-//  })
-
-
-//  remCart.forEach(minusSign => {
-//   minusSign.addEventListener("click", () => {
-//     input.value = parseInt(input.value) - 1;
-//   } )
-//  })
-
-  
-// console.log(input)
-
-
-
-
-
-// function volwelCheck(arr) {
-//   let count = 0;
- 
-   
-
-//   console.log(vowl);
-  
-//  return count;
-// }
-
-// console.log(volwelCheck("a", "d", "r", "u", "i", "e", "o"));
 
 
  
-function checkVowel(arr) {
-    let bags = ["a", "e", "i", "o", "u"]
-    let count = 0;
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < bags.length; j++) {
-            if(arr[i] == bags[j]) {
-                count++
-            }
+ 
+ 
+// FAVOURITE /HEART SECTION
+favCart.forEach(box => {
+    box.addEventListener('click', (event) => {
+      console.log('box clicked', event);
+      if(box.style.color == "black"){
+      box.setAttribute('style', 'color: red;');
+      }else {
+        box.setAttribute('style', 'color: black;');
+      }
+    });
+  });
+
+// FOR THE INCREEMENT BUTTON
+      plusBtn1.addEventListener("click", () => {
+        input1.value = parseInt(input1.value) + 1;
+      })
+      plusBtn2.addEventListener("click", () => {
+        input2.value = parseInt(input2.value) + 1;
+      })
+      plusBtn3.addEventListener("click", () => {
+        input3.value = parseInt(input3.value) + 1;
+      })
+
+      // DECREMENT BUTTON
+      minusBtn1.addEventListener("click", () => {
+        if(input1.value <= 1){
+          input1.value === 1;
+        }else{
+          input1.value = parseInt(input1.value) - 1;
         }
-    }
-    return count ;
-}
-
-console.log(checkVowel("aeiglo"));
-
-// function extractMiddle(str) {
-
-//     var position;
-//     var length;
-
-//     if(str.length % 2 == 1) {
-//         position = str.length / 2;
-//         length = 1;
-//     } else {
-//         position = str.length / 2 - 1;
-//         length = 2;
-//     }
-
-//     return str.substring(position, position + length)
-// }
-
-// console.log(extractMiddle("handbanann"));
-
-// function extractMiddle(str) {
-//     return str.substr(Math.ceil(str.length / 2 - 1), str.length % 2 === 0 ? 2 : 1);
-//   }
-
-//   console.log(extractMiddle("handbanann"));
-
-
-// Very simple, given an integer or a floating-point number, find its opposite.
-// function oppositeNum(num) {
-//     return -1 * 9;
-// }
+        
+      })
+      minusBtn1.addEventListener("click", () => {
+        if(input2.value <= 1){
+          input2.value === 1;
+        }else{
+          input2.value = parseInt(input2.value) - 1;
+        }
+        
+      })
+      minusBtn1.addEventListener("click", () => {
+        if(input3.value <= 1){
+          input3.value === 1;
+        }else{
+          input3.value = parseInt(input3.value) - 1;
+        }
+        
+      })
+      
+      console.log(thirdLine);
